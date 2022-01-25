2437bd4041de5bc723d57f50f02ba1a32c55bcb1
mauricekollewe <kollewem.35@gmail.com>
2022-01-25 21:49:41 +0000

    updates

--- src/App.js
+++ src/App.js
@@ -3,48 +3,69 @@ import axios from 'axios';
 import { Card, CardGroup } from 'react-bootstrap';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { File } from './Doc/File';
 
 
 const App = () => {
   const [current, setCurrent] = useState([]);
-  const [results, setResults] = useState([])
+  const [results, setResults] = useState([]);
 
-  
+
+
   useEffect(() => {
     axios
-    .all([
-      axios.get("https://covid2019-api.herokuapp.com/v2/total"),
-      axios.get("https://covid2019-api.herokuapp.com/v2/current")
-    ])
+      .all([
+        axios.get("https://covid2019-api.herokuapp.com/v2/total"),
+        axios.get("https://covid2019-api.herokuapp.com/v2/current"),
+      ])
       .then(res => {
         //  setting total results
         setCurrent(res[0].data.data);
         // setting results for all countries
-        setResults(res[1].data.data);
+        setResults(res[1].data.data, File);
+        console.log(res[1].data.data, File);
       })
       .catch(err => {
         console.log(err);
       });
   }, []);
+
+
   if (!results) return 'no data';
-    if (!Array.isArray(results)) return 'results are not array'
+  if (!Array.isArray(results)) return 'results are not array'
+  if (!File) return 'no data';
+  if (!Array.isArray(File)) return 'results are not array'
 
 
-  const locations = results.map((data) => {
+  const locations = results.map((data, i) => {
+     // let country = [];
+  if('data' == 'File') {
+    let country = 'location'
+    return country
+  }
+
     return (
       <Card
+        key={i}
         bg="secondary"
         text="white"
         className="text-center"
         style={{ margin: "5px" }}
       >
+        <Card.Img variant="top" 
+        src={data.flagUrl} />
         <Card.Body>
           <Card.Title>{data.location}</Card.Title>
           <Card.Text>Cases {data.cases}</Card.Text>
+          <Card.Text>Confirmed {data.confirmed}</Card.Text>
+          <Card.Text>Deaths {data.deaths}</Card.Text>
+          <Card.Text>Recovered {data.recovered}</Card.Text>
+          <Card.Text>Active {data.active}</Card.Text>
         </Card.Body>
       </Card>
     );
-    });
+  });
+
 
   return (
     <div>
@@ -61,22 +82,9 @@ const App = () => {
             <small>Last updated {new Date(current.data).toDateString()}</small>
           </Card.Footer>
         </Card>
-        <Card bg="success" text="white" className="text-center" style={{ margin: "5px" }}>
-          <Card.Body>
-            <Card.Title>
-              <small>location</small>
-            </Card.Title>
-            <Card.Title>Recovered</Card.Title>
-            <Card.Text>{current.recovered}</Card.Text>
-          </Card.Body>
-          <Card.Footer>
-            <small>Last updated 3 mins ago</small>
-          </Card.Footer>
-        </Card>
         <Card bg="danger" text="white" className="text-center" style={{ margin: "5px" }}>
           <Card.Body>
             <Card.Title>
-              <small>location</small>
             </Card.Title>
             <Card.Title>Deaths</Card.Title>
             <Card.Text>{current.deaths}</Card.Text>
@@ -85,10 +93,20 @@ const App = () => {
             <small>Last updated 3 mins ago</small>
           </Card.Footer>
         </Card>
+        <Card bg="success" text="white" className="text-center" style={{ margin: "5px" }}>
+          <Card.Body>
+            <Card.Title>
+            </Card.Title>
+            <Card.Title>Recovered</Card.Title>
+            <Card.Text>{current.recovered}</Card.Text>
+          </Card.Body>
+          <Card.Footer>
+            <small>Last updated 3 mins ago</small>
+          </Card.Footer>
+        </Card>
         <Card bg="primary" text="white" className="text-center" style={{ margin: "5px" }}>
           <Card.Body>
             <Card.Title>
-              <small>location</small>
             </Card.Title>
             <Card.Title>Active</Card.Title>
             <Card.Text>{current.active}</Card.Text>
---
+++ src/Doc/File.js
@@ -0,0 +1,208 @@
+
+
+export const File = ([
+	{
+		"country": 'US',
+		"flagURL": 'http://www.geonames.org/flags/l/us.gif',
+	},
+	{
+		"country": 'Andorra',
+		"flagUrl": 'https://img.geonames.org/flags/x/ad.gif',
+	},
+	{
+		"country": 'United Arab Emirates',
+		"flagUrl": 'https://img.geonames.org/flags/x/ae.gif',
+	},
+	{
+		country: 'Afghanistan',
+		flagUrl: 'https://img.geonames.org/flags/x/af.gif',
+	},
+	{
+		country: 'Antigua and Barbuda',
+		flagUrl: 'https://img.geonames.org/flags/x/ag.gif',
+	},
+	{
+		country: 'Anguilla',
+		flagUrl: 'https://img.geonames.org/flags/x/ai.gif',
+	},
+	{
+		country: 'Albania',
+		flagUrl: 'https://img.geonames.org/flags/x/al.gif',
+	},
+	{
+		country: 'Armenia',
+		flagUrl: 'https://img.geonames.org/flags/x/am.gif',
+	},
+	{
+		country: 'Angola',
+		flagUrl: 'https://img.geonames.org/flags/x/ao.gif',
+	},
+	{
+		country: 'Antarctica',
+		flagUrl: 'https://img.geonames.org/flags/x/aq.gif',
+	},
+	{
+		country: 'Argentina',
+		flagUrl: 'https://img.geonames.org/flags/x/ar.gif',
+	},
+	{
+		country: 'American Samoa',
+		flagUrl: 'https://img.geonames.org/flags/x/as.gif',
+	},
+	{
+		country: 'Austria',
+		flagUrl: 'https://img.geonames.org/flags/x/at.gif',
+	},
+	{
+		country: 'Australia',
+		flagUrl: 'https://img.geonames.org/flags/x/au.gif',
+	},
+	{
+		country: 'Aruba',
+		flagUrl: 'https://img.geonames.org/flags/x/aw.gif',
+	},
+	{
+		country: 'Åland',
+		flagUrl: 'https://img.geonames.org/flags/x/ax.gif',
+	},
+	{
+		country: 'Azerbaijan',
+		flagUrl: 'https://img.geonames.org/flags/x/az.gif',
+	},
+	{
+		country: 'United Arab Emirates',
+		flagUrl: 'https://img.geonames.org/flags/x/ae.gif',
+	},
+	{
+		country: 'United Arab Emirates',
+		flagUrl: 'https://img.geonames.org/flags/x/ae.gif',
+	},
+	{
+		country: 'United Arab Emirates',
+		flagUrl: 'https://img.geonames.org/flags/x/ae.gif',
+	},
+	{
+		country: 'United Arab Emirates',
+		flagUrl: 'https://img.geonames.org/flags/x/ae.gif',
+	},
+	{
+		country: 'United Arab Emirates',
+		flagUrl: 'https://img.geonames.org/flags/x/ae.gif',
+	},
+	{
+		country: 'United Arab Emirates',
+		flagUrl: 'https://img.geonames.org/flags/x/ae.gif',
+	},
+	{
+		country: 'United Arab Emirates',
+		flagUrl: 'https://img.geonames.org/flags/x/ae.gif',
+	},
+	{
+		country: 'United Arab Emirates',
+		flagUrl: 'https://img.geonames.org/flags/x/ae.gif',
+	},
+	{
+		country: 'United Arab Emirates',
+		flagUrl: 'https://img.geonames.org/flags/x/ae.gif',
+	},
+	{
+		country: 'United Arab Emirates',
+		flagUrl: 'https://img.geonames.org/flags/x/ae.gif',
+	},
+	{
+		country: 'United Arab Emirates',
+		flagUrl: 'https://img.geonames.org/flags/x/ae.gif',
+	},
+	{
+		country: 'United Arab Emirates',
+		flagUrl: 'https://img.geonames.org/flags/x/ae.gif',
+	},
+	{
+		country: 'United Arab Emirates',
+		flagUrl: 'https://img.geonames.org/flags/x/ae.gif',
+	},
+	{
+		country: 'United Arab Emirates',
+		flagUrl: 'https://img.geonames.org/flags/x/ae.gif',
+	},
+	{
+		country: 'United Arab Emirates',
+		flagUrl: 'https://img.geonames.org/flags/x/ae.gif',
+	},
+	{
+		country: 'United Arab Emirates',
+		flagUrl: 'https://img.geonames.org/flags/x/ae.gif',
+	},
+	{
+		country: 'United Arab Emirates',
+		flagUrl: 'https://img.geonames.org/flags/x/ae.gif',
+	},
+	{
+		country: 'United Arab Emirates',
+		flagUrl: 'https://img.geonames.org/flags/x/ae.gif',
+	},
+	{
+		country: 'United Arab Emirates',
+		flagUrl: 'https://img.geonames.org/flags/x/ae.gif',
+	},
+	{
+		country: 'United Arab Emirates',
+		flagUrl: 'https://img.geonames.org/flags/x/ae.gif',
+	},
+	{
+		country: 'United Arab Emirates',
+		flagUrl: 'https://img.geonames.org/flags/x/ae.gif',
+	},
+	{
+		country: 'United Arab Emirates',
+		flagUrl: 'https://img.geonames.org/flags/x/ae.gif',
+	},
+	{
+		country: 'United Arab Emirates',
+		flagUrl: 'https://img.geonames.org/flags/x/ae.gif',
+	},
+	{
+		country: 'United Arab Emirates',
+		flagUrl: 'https://img.geonames.org/flags/x/ae.gif',
+	},
+	{
+		country: 'United Arab Emirates',
+		flagUrl: 'https://img.geonames.org/flags/x/ae.gif',
+	},
+	{
+		country: 'United Arab Emirates',
+		flagUrl: 'https://img.geonames.org/flags/x/ae.gif',
+	},
+	{
+		country: 'United Arab Emirates',
+		flagUrl: 'https://img.geonames.org/flags/x/ae.gif',
+	},
+	{
+		country: 'United Arab Emirates',
+		flagUrl: 'https://img.geonames.org/flags/x/ae.gif',
+	},
+	{
+		country: 'United Arab Emirates',
+		flagUrl: 'https://img.geonames.org/flags/x/ae.gif',
+	},
+	{
+		country: 'United Arab Emirates',
+		flagUrl: 'https://img.geonames.org/flags/x/ae.gif',
+	},
+	{
+		country: 'United Arab Emirates',
+		flagUrl: 'https://img.geonames.org/flags/x/ae.gif',
+	},
+	{
+		country: 'United Arab Emirates',
+		flagUrl: 'https://img.geonames.org/flags/x/ae.gif',
+	},
+	{
+		country: 'United Arab Emirates',
+		flagUrl: 'https://img.geonames.org/flags/x/ae.gif',
+	},
+
+])
+
+
+
