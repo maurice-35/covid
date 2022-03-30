e9764e0eaa9538ca09fe271ce5e265b3c0026f8e
mauricekollewe <kollewem.35@gmail.com>
2022-03-30 11:57:45 +0100

    updates

--- src/Search.js
+++ src/Search.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React, { useState } from 'react'
 import SearchIcon from '@material-ui/icons/Search'
 import CloseIcon from '@material-ui/icons/Close'
@@ -81,6 +80,3 @@ const Search = ({ placeholder, data }) => {
 }
 
 export default Search
-=======
-
->>>>>>> 241063133f106bb70ad05a53c05fc8fc32b1aa6e
