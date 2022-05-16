6db3ed9071e3f8181e79a1fc2aeac5fb0cab9012
mauricekollewe <kollewem.35@gmail.com>
2022-05-16 15:34:20 +0100

    updates

--- src/App.js
+++ src/App.js
@@ -1,46 +1,47 @@
-import React, { useState, useEffect } from 'react'
-import axios from 'axios'
-import { Card, CardGroup } from 'react-bootstrap'
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { Card, CardGroup } from "react-bootstrap";
 
-import 'bootstrap/dist/css/bootstrap.min.css'
-import { File } from './Doc/File'
-import Search from './Search'
-
-
+import "bootstrap/dist/css/bootstrap.min.css";
+import { File } from "./Doc/File";
+import Search from "./Search";
 
 const App = () => {
-  const [current, setCurrent] = useState([])
-  const [results, setResults] = useState([])
+  const [current, setCurrent] = useState([]);
+  const [results, setResults] = useState([]);
 
   useEffect(() => {
-    axios.all([
-      axios.get('https://api.apify.com/v2/key-value-stores/SmuuI0oebnTWjRTUh/records/LATEST?disableRedirect=true'),
-      axios.get('https://api.apify.com/v2/key-value-stores/tVaYRsPHLjNdNBu7S/records/LATEST?disableRedirect=true'),
-    ])
-      .then(res => {
+    axios
+      .all([
+        axios.get(
+          "https://api.apify.com/v2/key-value-stores/SmuuI0oebnTWjRTUh/records/LATEST?disableRedirect=true"
+        ),
+        axios.get(
+          "https://api.apify.com/v2/key-value-stores/tVaYRsPHLjNdNBu7S/records/LATEST?disableRedirect=true"
+        ),
+      ])
+      .then((res) => {
         //  setting world total results
         setCurrent(res[0].data.regionData[0]);
         // setting results for some countries
         setResults(res[1].data, File);
       })
-      .catch(err => {
+      .catch((err) => {
         console.log(err);
-      })
-  }, [])
+      });
+  }, []);
 
-  let flagCount = 0
+  let flagCount = 0;
   // eslint-disable-next-line
   results.map((coun) => {
-    const newResults = { ...results[flagCount], ...File[flagCount] }
-    results[flagCount] = newResults
-    flagCount += 1
-  })
+    const newResults = { ...results[flagCount], ...File[flagCount] };
+    results[flagCount] = newResults;
+    flagCount += 1;
+  });
 
-  let count = 0
+  let count = 0;
 
   const countries = results.map((data, id) => {
-
-
     return (
       <Card
         key={id}
@@ -51,7 +52,7 @@ const App = () => {
       >
         {File[count].country}
         <img src={File[count].flagUrl} alt="flag" />
-        {count = count += 1}
+        {(count = count += 1)}
         <Card.Body>
           <Card.Title> {data.country}</Card.Title>
           <Card.Text>Cases</Card.Text>
@@ -59,25 +60,41 @@ const App = () => {
           <Card.Text>Deceased {data.deceased}</Card.Text>
           <Card.Text>Recovered {data.recovered}</Card.Text>
           <Card.Text>Tested {data.tested}</Card.Text>
-          <Card.Text><small>Last updated {new Date().toString()}</small></Card.Text>
+          <Card.Text>
+            <small>Last updated {new Date().toString()}</small>
+          </Card.Text>
         </Card.Body>
       </Card>
-    )
-  })
-
+    );
+  });
 
   return (
     <div>
       <div className="world">
         <Card.Title>
           <Card.Title></Card.Title>
-          <Card.Title><img src="https://phil.cdc.gov//PHIL_Images/23312/23312_lores.jpg" alt="img" /></Card.Title>
+          <Card.Title>
+            <img
+              src="https://phil.cdc.gov//PHIL_Images/23312/23312_lores.jpg"
+              alt="img"
+            />
+          </Card.Title>
           <Card.Title>World Covid-19 Data</Card.Title>
-          <Card.Title><img src="https://phil.cdc.gov//PHIL_Images/23312/23312_lores.jpg" alt="img" /></Card.Title>
+          <Card.Title>
+            <img
+              src="https://phil.cdc.gov//PHIL_Images/23312/23312_lores.jpg"
+              alt="img"
+            />
+          </Card.Title>
         </Card.Title>
       </div>
       <CardGroup id="background">
-        <Card bg="secondary" text="white" className="text-center" style={{ margin: "5px" }}>
+        <Card
+          bg="secondary"
+          text="white"
+          className="text-center"
+          style={{ margin: "5px" }}
+        >
           <Card.Body>
             <Card.Title>World Active Cases</Card.Title>
             <Card.Text>{current.activeCases}</Card.Text>
@@ -86,10 +103,14 @@ const App = () => {
             <small>Last updated {new Date().toString()}</small>
           </Card.Footer>
         </Card>
-        <Card bg="primary" text="white" className="text-center" style={{ margin: "5px" }}>
+        <Card
+          bg="primary"
+          text="white"
+          className="text-center"
+          style={{ margin: "5px" }}
+        >
           <Card.Body>
-            <Card.Title>
-            </Card.Title>
+            <Card.Title></Card.Title>
             <Card.Title>World Total Cases</Card.Title>
             <Card.Text>{current.totalCases}</Card.Text>
           </Card.Body>
@@ -97,10 +118,14 @@ const App = () => {
             <small>Last updated {new Date().toString()}</small>
           </Card.Footer>
         </Card>
-        <Card bg="success" text="white" className="text-center" style={{ margin: "5px" }}>
+        <Card
+          bg="success"
+          text="white"
+          className="text-center"
+          style={{ margin: "5px" }}
+        >
           <Card.Body>
-            <Card.Title>
-            </Card.Title>
+            <Card.Title></Card.Title>
             <Card.Title>World Total Recovered</Card.Title>
             <Card.Text>{current.totalRecovered}</Card.Text>
           </Card.Body>
@@ -108,10 +133,14 @@ const App = () => {
             <small>Last updated {new Date().toString()}</small>
           </Card.Footer>
         </Card>
-        <Card bg="danger" text="white" className="text-center" style={{ margin: "5px" }}>
+        <Card
+          bg="danger"
+          text="white"
+          className="text-center"
+          style={{ margin: "5px" }}
+        >
           <Card.Body>
-            <Card.Title>
-            </Card.Title>
+            <Card.Title></Card.Title>
             <Card.Title>World Total Deaths</Card.Title>
             <Card.Text>{current.totalDeaths}</Card.Text>
           </Card.Body>
@@ -119,12 +148,19 @@ const App = () => {
             <small>Last updated {new Date().toString()}</small>
           </Card.Footer>
         </Card>
-
       </CardGroup>
       <Search placeholder="Search" data={results} />
-      <CardGroup style={{ display: 'grid', gridTemplateColumns: 'repeat(3, 1fr)', gridGap: 10 }}>{countries}</CardGroup>
+      <CardGroup
+        style={{
+          display: "grid",
+          gridTemplateColumns: "repeat(3, 1fr)",
+          gridGap: 10,
+        }}
+      >
+        {countries}
+      </CardGroup>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- src/Search.js
+++ src/Search.js
@@ -1,75 +1,75 @@
-import React, { useState } from 'react'
-import SearchIcon from '@material-ui/icons/Search'
-import CloseIcon from '@material-ui/icons/Close'
-import { Card } from 'react-bootstrap'
-import 'bootstrap/dist/css/bootstrap.min.css'
-
+import React, { useState } from "react";
+import SearchIcon from "@material-ui/icons/Search";
+import CloseIcon from "@material-ui/icons/Close";
+import { Card } from "react-bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 const Search = ({ placeholder, data }) => {
-	const [filteredData, setFilteredData] = useState([])
-	const [countryEntered, setCountryEntered] = useState("")
-	
-	const handleFilter = (e) => {
-		const searchCountry = e.target.value
-		setCountryEntered(searchCountry)
-		const newFilter = data.filter((val) => {
-			return val.country.toLowerCase().includes(searchCountry.toLowerCase())
-		})
+  const [filteredData, setFilteredData] = useState([]);
+  const [countryEntered, setCountryEntered] = useState("");
 
-		if (searchCountry  === "") {
-			setFilteredData([])
-		} else {
-			setFilteredData(newFilter)
-		}
-		console.log(searchCountry)
-		console.log(newFilter)
-	}
-	
-	return (
-		<div className="search">
-			<div className="searchInput">
-				<input
-					type="text"
-					placeholder={placeholder}
-					value={countryEntered}
-					onChange={handleFilter}
-				/>
-				<div className="searchIcon">
-					{filteredData.length === 0 ? (
-						<SearchIcon />
-					) : (
-						<CloseIcon id="clearBtn" />
-					)}
-				</div>
-			</div>
-			{data.length !== 0 && (
-				<div className="result">
-					{filteredData.map((value, id) => {
-						return (
-							<Card
-								key={id}
-								bg="secondary"
-								text="white"
-								className="text-center"
-								style={{ width: "60vw" }}
-								onClick={handleFilter}>
-								{value.location}
-								<img src={value.flagUrl} alt="flag" />
-								<Card.Body>
-									<Card.Title> {value.country}</Card.Title>
-									<Card.Text>Cases</Card.Text>
-									<Card.Text>Infected {value.infected}</Card.Text>
-									<Card.Text>Deceased {value.deceased}</Card.Text>
-									<Card.Text>Recovered {value.recovered}</Card.Text>
-									<Card.Text>Tested {value.tested}</Card.Text>
-								</Card.Body>
-							</Card>
-						)
-					})}
-				</div>
-			)}
-		</div>
-	)
-}
+  const handleFilter = (e) => {
+    const searchCountry = e.target.value;
+    setCountryEntered(searchCountry);
+    const newFilter = data.filter((val) => {
+      return val.country.toLowerCase().includes(searchCountry.toLowerCase());
+    });
 
-export default Search
+    if (searchCountry === "") {
+      setFilteredData([]);
+    } else {
+      setFilteredData(newFilter);
+    }
+    // console.log(searchCountry)
+    // console.log(newFilter)
+  };
+
+  return (
+    <div className="search">
+      <div className="searchInput">
+        <input
+          type="text"
+          placeholder={placeholder}
+          value={countryEntered}
+          onChange={handleFilter}
+        />
+        <div className="searchIcon">
+          {filteredData.length === 0 ? (
+            <SearchIcon />
+          ) : (
+            <CloseIcon id="clearBtn" />
+          )}
+        </div>
+      </div>
+      {data.length !== 0 && (
+        <div className="result">
+          {filteredData.map((value, id) => {
+            return (
+              <Card
+                key={id}
+                bg="secondary"
+                text="white"
+                className="text-center"
+                style={{ width: "60vw" }}
+                onClick={handleFilter}
+              >
+                {value.location}
+                <img src={value.flagUrl} alt="flag" />
+                <Card.Body>
+                  <Card.Title> {value.country}</Card.Title>
+                  <Card.Text>Cases</Card.Text>
+                  <Card.Text>Infected {value.infected}</Card.Text>
+                  <Card.Text>Deceased {value.deceased}</Card.Text>
+                  <Card.Text>Recovered {value.recovered}</Card.Text>
+                  <Card.Text>Tested {value.tested}</Card.Text>
+                </Card.Body>
+              </Card>
+            );
+          })}
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default Search;
